b48cbdc583d9b9253de29ac36295983f8772f7e2
anton <vao@asu-vei.ru>
2025-08-07 15:04:56 +0300

    [-] warning

--- zip_open.c
+++ zip_open.c
@@ -374,7 +374,7 @@ static bool _zip_read_cdir(zip_t *za, zip_buffer_t *buffer, zip_uint64_t buf_off
                 return true;
             }
             if (tail_len < comment_len) {
-                comment_len = tail_len;
+                comment_len = (zip_uint16_t) tail_len;
             }
         }
 
