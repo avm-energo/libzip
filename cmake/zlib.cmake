665cf2748da8947da9e61685f6375b9781437938
anton <vao@asu-vei.ru>
2026-01-13 13:27:05 +0300

    [^] zlib to 1.4.3

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(avm-zlib
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/zlib.git
-  GIT_TAG           v1.4.2
+  GIT_TAG           v1.4.3
 )
 
 FetchContent_MakeAvailable(avm-zlib)
